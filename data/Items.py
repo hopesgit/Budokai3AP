e7407cecbea96359a2c4783cfd80a4624d16c982
Hope Gochnour <hope.gochnour@gmail.com>
2025-07-13 03:40:12 -0600

    Add the rest of the game's items to data/items.py

--- data/Items.py
+++ data/Items.py
@@ -16,19 +16,12 @@ class ItemData(ABC):
         self.item_id = id
         self.name = name
 
-
-def get_capsule_color(item: ItemData) -> int:
-    if item.item_id < 400:
-        return 1
-    elif item.item_id < 500:
-        return 2
-    elif item.item_id < 600:
-        return 3
-    else: return 4
+class Item(ItemData):
+    pass
 
 
 @dataclass
-class Capsule(ItemData):
+class Capsule(Item):
     offset: int
     capsule_color: int
     stacks: bool
@@ -146,7 +139,7 @@ BREAK_KRILLIN = RedCapsule(69, "Breakthrough (Krillin)")
 ## Piccolo
 SYNC_NAIL = RedCapsule(70, "Sync With Nail")
 FUSE_KAMI = RedCapsule(71, "Fuse With Kami")
-DWAVE = RedCapsule(72, "Destructive Wave", stacks=True)
+DEST_WAVE = RedCapsule(72, "Destructive Wave", stacks=True)
 LGRENADE = RedCapsule(73, "Light Grenade", stacks=True)
 SBEAM_CANNON = RedCapsule(74, "Special Beam Cannon", stacks=True)
 HZGRENADE = RedCapsule(75, "Hellzone Grenade", stacks=True)
@@ -317,6 +310,85 @@ BREAK_SAIBA = RedCapsule(188, "Breakthrough (Saibaman)")
 ## Cell Jr
 KAME_CELLJR = RedCapsule(189, "Kamehameha (Cell Jr)", stacks=True)
 BREAK_CELLJR = RedCapsule(190, "Breakthrough (Cell Jr)")
+
+RED_CAPSULES = [
+    # Goku
+    KAIOKEN, SSJ_GOKU, SSJ2_GOKU, SSJ3_GOKU, SSJ4_GOKU, KAME_GOKU, DRAG_FIST_GOKU, WARP_KAME_GOKU, SPIRIT_BOMB_GOKU,
+    GOGETA_GOKU, SSJ4_GOGETA_GOKU, VEGITO_GOKU, BREAK_GOKU,
+    # Kid Gohan
+    POTENTIAL_KGOHAN, MASENKO, BREAK_KGOHAN,
+    # Teen Gohan
+    SSJ_TGOHAN, SSJ2_TGOHAN, KAME_TGOHAN, SOAR_TGOHAN, FS_KAME, BREAK_TGOHAN,
+    # Gohan
+    SSJ_GOHAN, SSJ2_GOHAN, ELDER_UNLOCK, KAME_GOHAN, SOAR_GOHAN, SUPER_KAME_GOHAN, BREAK_GOHAN,
+    # Gt Saiyaman
+    JPUNCH, JKICK, JPOSE, BREAK_GTS,
+    # Goten
+    SSJ_GOTEN, KAME_GOTEN, CHARGE, GOTENKS_GOTEN, BREAK_GOTEN,
+    # Vegeta
+    SSJ_VEGETA, SSJ2_VEGETA, SSJ4_VEGETA, GALICK, FINAL_IMPACT, FINAL_FLASH, BIG_BANG, GOGETA_VEGETA,
+    SSJ4_GOGETA_VEGETA, VEGITO_VEGETA, BREAK_VEGETA,
+    # Trunks
+    SSJ_TRUNKS, SSJ2_TRUNKS, FINISH_BUSTER, BUSTER_CANNON, BURNING_SLASH, BREAK_TRUNKS,
+    # Kid Trunks
+    SSJ_KTRUNKS, DOUBLE_BUSTER, FINAL_CANNON, GOTENKS_KTRUNKS, BREAK_KTRUNKS,
+    # Krillin
+    POTENTIAL_KRILLIN, KAME_KRILLIN, DD_KRILLIN, FIERCE_DD, BREAK_KRILLIN,
+    # Piccolo
+    SYNC_NAIL, FUSE_KAMI, DEST_WAVE, LGRENADE, SBEAM_CANNON, HZGRENADE, BREAK_PICCOLO,
+    # Tien
+    DODON, BLAST_CANNON, NEO_BLAST_CANNON, BREAK_TIEN,
+    # Yamcha
+    KAME_YAMCHA, WOLF_FANG, SPIRIT_BALL, BREAK_YAMCHA,
+    # Hercule
+    TENSION, DYNAMITE_KICK, ROLLING_PUNCH, HERC_SPECIAL, PRESENT, BREAK_HERCULE,
+    # Videl
+    EAGLE_KICK, HAWK_ARROW, CLOSE_CALL, BREAK_VIDEL,
+    # Supreme Kai
+    SHOCKWAVE, ABILITIES, KIBITOKAI, BREAK_SK,
+    # Uub
+    KI_CANNON, FIERCE_FLURRY, BREAK_UUB,
+    # Raditz
+    SUNDAY, SATURDAY, BREAK_RADITZ,
+    # Nappa
+    BOMBER_DX, BREAK_CANNON, GIANT_STORM, BREAK_NAPPA,
+    # Ginyu
+    MILKY_CANNON, STRONG_JERSEY, SFP1, SFP2, BODY_CHANGE, BREAK_GINYU,
+    # Recoome
+    ERASER_GUN, REC_KICK, SFP3, SFP4, REC_BOMBER, BREAK_REC,
+    # Frieza
+    SECOND_FORM, THIRD_FORM, FINAL_FORM_FRIEZA, HUNDRED_PERCENT, DEATH_BEAM, DEATH_WAVE, DEATH_BALL, BREAK_FRIEZA,
+    # 16
+    ROCKET_PUNCH, HELL_FLASH, BREAK_16,
+    # 17
+    POWER_BLITZ_17, ENERGY_FIELD_17, ACCEL_DANCE_17, BREAK_17,
+    # 18
+    POWER_BLITZ_18, DD_18, ACCEL_DANCE_18, BREAK_18,
+    # Dr Gero
+    PHOTON_WAVE, KI_ABSORB, LIFE_DRAIN, BREAK_GERO,
+    # Cell
+    ABSORB_17, PERFECT_FORM, SUPER_PERFECT, KAME_CELL, ENERGY_FIELD_CELL, SPIRIT_BOMB_CELL, BREAK_CELL,
+    # Majin Buu
+    INN_CANNON, INN_EXPRESS, ANGRY_EXPLODE, BREAK_MBUU,
+    # Super Buu
+    ABSORB, ILL_FLASH, ILL_BALL, BREAK_SBUU,
+    # Kid Buu
+    KAME_KBUU, VANISH_BALL, WARP_KAME_KBUU, BREAK_KBUU,
+    # Dabura
+    DEMON_WILL, HELL_BLITZ, EVIL_BLAST, HELL_RUSH, BREAK_DABURA,
+    # Cooler
+    FINAL_FORM_COOLER, DESTRUCT_RAY, SAUZER_BLADE, SUPERNOVA, BREAK_COOLER,
+    # Bardock
+    RIOT_JAVELIN, HEAT_PHALANX, SPIRIT_SAIYANS, BREAK_BARDOCK,
+    # Broly
+    LSSJ_BROLY, BLASTER_SHELL, GIGANT_PRESS, GIGANT_METEOR, BREAK_BROLY,
+    # Omega
+    DTHUNDER, WHIRLWIND, MINUS_BALL, BREAK_OMEGA,
+    # Saiba
+    ACID, SELFDESTRUCT, BREAK_SAIBA,
+    # Cell Jr
+    KAME_CELLJR, BREAK_CELLJR
+]
 
 # Equipment Capsules (Green)
 ## Attack up
@@ -514,4 +586,275 @@ WE_GOTTA_POWER_EE = GreenCapsule(363, "WE GOTTA POWER!!")
 WE_GOTTA_POWER_EEE = GreenCapsule(364, "WE GOTTA POWER!!!")
 WE_GOTTA_POWER_EEEE = GreenCapsule(365, "WE GOTTA POWER!!!!")
 
+GREEN_CAPSULES = [
+    Z_SWORD, JUICE, DAIMAOS_POWER, FRUITS_TRAINING, VIDELS_KISS, KIBITOS_BACKING, BATTLE_TESTAMENT, POWER_AMP,
+    WARRIOR_GENETICS, DEMON_REALM_FLAMES, KAKAROTS_CRYING, GINYU_SPECIAL_FORCES, COOLER_ARMORED_SQUAD, POWER_FRIENDS,
+    APPETITES_MEN, STRENGTH_SERUM, KINGS_LINEAGE, CHEERING,
+    GENERAL_VEST, TRAINING_VEST, STURDY_VEST, MYSTERIOUS_VEST, VEST_GPA_GOHAN, VEST_HOLE_TAIL, TURTLE_VEST, KAMI_VEST,
+    TRIBE_UNIFORM, E_TRAINING_UNIFORM, E_STURDY_UNIFORM, E_MYSTERY_UNIFORM, NORMAL_JACKET, QUALITY_JACKET,
+    STURDY_JACKET, MYSTERY_JACKET, KAMI_OUTFIT, KINGKAI_OUTFIT, GRANDKAI_OUTFIT, SUPREMEKAI_OUTFIT, OLD_TRAINING_VEST,
+    WEDDING_VEST, CHAMP_VEST, HIGHTECH_VEST, CHAMPION_BELT, TSHIRT, BLACK_BELT_VEST, SPARRING_OUTFIT, GTS_WARDROBE,
+    OLD_STYLE_ARMOR, RIT_ARMOR, NEW_STYLE_ARMOR, BULMAS_ARMOR, SPECIAL_COATING, IMP_SPECIAL_COATING, NANOMACHINE,
+    IMPROVE_NANO, LIFE_EXTRACT_10, LIFE_EXTRACT_100, LIFE_EXTRACT_1000, LIFE_EXTRACT_10000, DEMON_REALM_GUARD,
+    MAGE_GUARD, BABIDIS_GUARD, BIBIDIS_GUARD, NORMAL_BELT, TRAINING_BELT, WARRIOR_BELT, MAJIN_BELT, LOW_CLASS_GUARD,
+    KANASSAN_GUARD, BATTLE_JACKET_PROTO, PATCHED_BATTLE_JACKET, STRONG_BODY_WRAP, KING_BODY_WRAP, GOD_BODY_WRAP,
+    LEGEND_BODY_WRAP, SHENRONS_HIDE, PORUNGAS_HIDE, SHADOW_DRAGONS_HIDE, SERUM_2X, SERUM_16X, SERUM_64X, SERUM_128X,
+    MIXED_BLOOD_POWER, MOON_LIGHT, FULL_MOON_GLOW, POTENTIAL_BOOST, UNIVERSAL_POWER, MIRACLE_POWER, ULTIMATE_POWER,
+    KINGS_CONFIDENCE, MODE_SWITCH_SYS, SAIYANS_AWAKEN, WAR_RACE_AWAKEN, NATURE_EVIL, HATRED_KAKAROT, BLACK_DRAGONBALL,
+    TOXIC_CHOCOLATE, RAGE_E, RAGE_EE, RAGE_EEE, SPIRIT_E, SPIRIT_EE, SPIRIT_EEE, SERIOUS_E, SERIOUS_EE, SERIOUS_EEE,
+    POWER_NEAR_LIMIT, DESPERATE_RES, DESPERATE_POWER, PRIDE_STRONGEST, OUNCE_STRENGTH, PRESSURE_CHAMP, DABURA_COOKIE,
+    PICCOLOS_REGEN, BUUS_REGEN, DENDES_RECOVERY, KIBITOS_REVIVAL, MEDICAL_MACHINE, AUTO_RESTORE, SAIYAN_SPIRIT,
+    GOING_ALL_OUT, GINYU_FORCE_BADGE, HERC_FALSE_COURAGE, RUSH_EEE, OVERTENSION, GERO_DEFLECT, GERO_DEFLECT_BACK,
+    GERO_ENERGY, VIRAL_HEART_DISEASE, FRIEZA_SPACESHIP, COOLER_SPACESHIP, BABIDI_MIND_CONTROL, BABIDI_SCOPE,
+    WARRIOR_CAREER, POWER_SAVE_SYS, BREATH_ROOM_STRONG, PARAGUS_ADMON, EVIL_GRIN, MEDITATION, YAKON, ANGEL_HALO,
+    HUMAN_CANDY, MARRONS_WISH, CHIAOTZUS_WISH, PUARS_WISH, CHICHIS_WISH, DENDES_WISH, BULMAS_WISH, WORLDS_EXPECT,
+    LOYAL_FRIEZA, UNIVERSAL_AMB, PRIDE_CLAN, ANDROID_GOALS, ESSENCE_MIGHTY, LOYAL_BABIDI, KIBITOS_WISH, KINGKAIS_WISH,
+    THIRST_EARTH_DESTRUCT, THOUGHTS_FRIENDS, GOD_DEST_ARROGANCE, GPA_GOHANS_TEACHINGS, GOKUS_TEACHINGS, TURTLE_SHELL,
+    CONCENTRATION, SPARKING_E, SPARKING_EE, SPARKING_EEE, SPARKING_EEEE, SPARKING_EEEEE, SPARKING_EEEEEE,
+    SPARKING_EEEEEEE, WE_GOTTA_POWER_E, WE_GOTTA_POWER_EE, WE_GOTTA_POWER_EEE, WE_GOTTA_POWER_EEEE
+]
+
 # Item Capsules (Yellow)
+TEMPURA = YellowCapsule(366, "Tempura Bowl")
+TONKATSU = YellowCapsule(367, "Tonkatsu (Fried Pork) Bowl")
+CHICKEN_EGG = YellowCapsule(368, "Chicken & Egg Bowl")
+CHILLED = YellowCapsule(369, "Chilled Juice")
+WELL_CHILLED = YellowCapsule(370, "Well Chilled Juice")
+EXTREME_CHILLED = YellowCapsule(371, "Extremely Chilled Juice")
+THIRD_SENZU = YellowCapsule(372, "1/3 Senzu Bean")
+HALF_SENZU = YellowCapsule(373, "1/2 Senzu Bean")
+SENZU_BEAN = YellowCapsule(374, "Senzu Bean" )
+KINGKAIS_WATER = YellowCapsule(375, "King Kai's Water")
+SUPREMEKAIS_WATER = YellowCapsule(376, "Supreme Kai's Water")
+GRANDKAIS_WATER = YellowCapsule(377, "Grand Supreme Kai's Water")
+HOLY_WATER_DROP = YellowCapsule(378, "Super Holy Water Drop")
+HOLY_WATER_BOTTLE = YellowCapsule(379, "Super Holy Water Bottle")
+HOLY_WATER = YellowCapsule(380, "Super Holy Water")
+HERC_DRINK = YellowCapsule(381, "Hercule Drink")
+HERC_DRINK_DX = YellowCapsule(382, "Hercule Drink DX")
+HERC_DRINK_SP = YellowCapsule(383, "Hercule Drink SP")
+KAMI_WATER_DROP = YellowCapsule(384, "Super Kami Water Drop")
+KAMI_WATER_BOTTLE = YellowCapsule(385, "Super Kami Water Bottle")
+KAMI_WATER = YellowCapsule(386, "Super Kami Water")
+PSHIELD_PROTO = YellowCapsule(387, "Portable Shield (Prototype)")
+PSHIELD_IMPROVED = YellowCapsule(388, "Portable Shield (Improved)" )
+PSHIELD_PROD = YellowCapsule(389, "Portable Shield (Production)" )
+PORT_BARRIER = YellowCapsule(390, "Portable Barrier System")
+GERO_DEF_SYS = YellowCapsule(391, "Gero Style Defense System")
+GERO_BARRIER_SYS = YellowCapsule(392, "Gero Style Barrier System")
+BIBIDIS_POT = YellowCapsule(393, "Bibidi's Pot")
+VACCINE = YellowCapsule(394, "Vaccine")
+SENZU_ROOT = YellowCapsule(395, "Senzu Root")
+SENZU_LEAF = YellowCapsule(396, "Senzu Leaf")
+SENZU_SEEDLING = YellowCapsule(397, "Senzu Seedling")
+EEL_SOUP = YellowCapsule(398, "Centipede Eel Soup")
+CF_TOAD = YellowCapsule(399, "Chicken-fried 7-Seasoned Toad")
+PAOZU_TAIL = YellowCapsule(400, "Paozusaurus Tail" )
+
+YELLOW_CAPSULES = [
+    TEMPURA, TONKATSU, CHICKEN_EGG, CHILLED, WELL_CHILLED, EXTREME_CHILLED, THIRD_SENZU, HALF_SENZU, SENZU_BEAN,
+    KINGKAIS_WATER, SUPREMEKAIS_WATER, GRANDKAIS_WATER, HOLY_WATER_DROP, HOLY_WATER_DROP, HOLY_WATER_BOTTLE, HOLY_WATER,
+    HERC_DRINK, HERC_DRINK_DX, HERC_DRINK_SP, KAMI_WATER_DROP, KAMI_WATER_BOTTLE, KAMI_WATER, PSHIELD_PROTO,
+    PSHIELD_IMPROVED, PSHIELD_PROD, PORT_BARRIER, GERO_DEF_SYS, GERO_BARRIER_SYS, BIBIDIS_POT, VACCINE, SENZU_ROOT,
+    SENZU_LEAF, SENZU_SEEDLING, EEL_SOUP, CF_TOAD, PAOZU_TAIL
+]
+
+# System Capsules (Gray)
+GOKU = GrayCapsule(401, "Goku")
+KID_GOKU = GrayCapsule(402, "Kid Goku")
+KID_GOHAN = GrayCapsule(403, "Kid Gohan")
+TEEN_GOHAN = GrayCapsule(404, "Teen Gohan")
+GOHAN = GrayCapsule(405, "Gohan")
+GT_SAIYAMAN = GrayCapsule(406, "Great Saiyaman")
+GOTEN = GrayCapsule(407, "Goten")
+VEGETA = GrayCapsule(408, "Vegeta")
+TRUNKS = GrayCapsule(409, "Trunks")
+KID_TRUNKS = GrayCapsule(410, "Kid Trunks")
+KRILLIN = GrayCapsule(411, "Krillin")
+PICCOLO = GrayCapsule(412, "Piccolo")
+TIEN = GrayCapsule(413,"Tien" )
+YAMCHA = GrayCapsule(414, "Yamcha")
+HERCULE = GrayCapsule(415, "Hercule")
+VIDEL = GrayCapsule(416, "Videl")
+SUPREME_KAI = GrayCapsule(417, "Supreme Kai")
+UUB = GrayCapsule(418, "Uub")
+RADITZ = GrayCapsule(419, "Raditz")
+NAPPA = GrayCapsule(420, "Nappa")
+GINYU = GrayCapsule(421, "Captain Ginyu")
+RECOOME = GrayCapsule(422, "Recoome")
+FRIEZA = GrayCapsule(423, "Frieza")
+ANDROID_16 = GrayCapsule(424, "Android 16")
+ANDROID_17 = GrayCapsule(425, "Android 17")
+ANDROID_18 = GrayCapsule(426, "Android 18" )
+DR_GERO = GrayCapsule(427, "Dr Gero" )
+CELL = GrayCapsule(428, "Cell")
+MAJIN_BUU = GrayCapsule(429, "Majin Buu")
+SUPER_BUU = GrayCapsule(430, "Super Buu")
+KID_BUU = GrayCapsule(431, "Kid Buu")
+DABURA = GrayCapsule(432, "Dabura")
+COOLER = GrayCapsule(433, "Cooler")
+BARDOCK = GrayCapsule(434, "Bardock")
+BROLY = GrayCapsule(435, "Broly")
+OMEGA_SHENRON = GrayCapsule(436, "Omega Shenron")
+SAIBAMAN = GrayCapsule(437, "Saibaman")
+CELL_JR = GrayCapsule(438, "Cell Jr")
+TRAINING_1 = GrayCapsule(439, "Training 1 Scouter ")
+TRAINING_2 = GrayCapsule(440, "Training 2 Fighting Basics")
+TRAINING_3 = GrayCapsule(441, "Training 3 Ki Control")
+TRAINING_4 = GrayCapsule(442, "Training 4 Death-moves")
+TRAINING_5 = GrayCapsule(443, "Training 5 Ki Control 2")
+TRAINING_6 = GrayCapsule(444, "Training 6 Dodging")
+TRAINING_7 = GrayCapsule(445, "Training 7 Teleporting")
+TRAINING_8 = GrayCapsule(446, "Training 8 Hi-level Fighting")
+TRAINING_9 = GrayCapsule(447, "Training 9 Ultimate Moves")
+TRAINING_10 = GrayCapsule(448, "Training 10 Dragon Rush")
+TRAINING_11 = GrayCapsule(449, "Training 11 Item Skills")
+TRAINING_12 = GrayCapsule(450, "Training 12 Final Secrets")
+GREEN_CARD = GrayCapsule(451, "Green Membership Card")
+SILVER_CARD = GrayCapsule(452, "Silver Membership Card")
+GOLD_CARD = GrayCapsule(453, "Gold Membership Card")
+BLACK_CARD = GrayCapsule(454, "Black Membership Card")
+TOURNEY_NOVICE = GrayCapsule(455, "World Tournament - Novice")
+TOURNEY_ADEPT = GrayCapsule(456, "World Tournament - Adept")
+TOURNEY_ADV = GrayCapsule(457, "World Tournament - Advanced")
+TOURNEY_CELL = GrayCapsule(458, "World Tournament - Cell Games")
+DRAGON_ARENA = GrayCapsule(459, "Dragon Arena Ticket")
+TOURNEY_STAGE = GrayCapsule(460, "World Tournament Stage")
+TIME_CHAMBER = GrayCapsule(461, "Hyperbolic Time Chamber")
+ARCHIPELAGO = GrayCapsule(462, "Archipelago")
+MOUNTAINS = GrayCapsule(463, "Mountains")
+PLAINS = GrayCapsule(464, "Plains")
+GPA_GOHANS_HOUSE = GrayCapsule(465, "Grandpa Gohan's House")
+NAMEK = GrayCapsule(466, "Planet Namek")
+CELL_RING = GrayCapsule(467, "Cell Ring")
+SUPREME_KAIS_WORLD = GrayCapsule(468, "Supreme Kai's World")
+INSIDE_BUU = GrayCapsule(469, "Inside Buu")
+RED_RIBBON_BASE = GrayCapsule(470, "Red Ribbon Base")
+GOKUS_WISH = GrayCapsule(471, "Goku's Wish")
+PATH_POWER = GrayCapsule(472, "The Path to Power")
+ENDLESS_PATH = GrayCapsule(473, "The Endless Path to Power")
+STRONGEST_TROPHY = GrayCapsule(474, "Strongest of Universe Trophy")
+MEMORIES_GOKU = GrayCapsule(475, "Memories of Goku")
+MEMORIES_PICCOLO = GrayCapsule(476, "Memories of Piccolo")
+MEMORIES_KID_GOHAN = GrayCapsule(477, "Memories of Kid Gohan")
+MEMORIES_TEEN_GOHAN = GrayCapsule(478, "Memories of Teen Gohan")
+MEMORIES_GOHAN = GrayCapsule(479, "Memories of Gohan")
+MEMORIES_VEGETA = GrayCapsule(480, "Memories of Vegeta")
+MEMORIES_GOTEN = GrayCapsule(481, "Memories of Goten")
+MEMORIES_TRUNKS = GrayCapsule(482, "Memories of Trunks")
+MEMORIES_KID_TRUNKS = GrayCapsule(483, "Memories of Kid Trunks")
+MEMORIES_KRILLIN = GrayCapsule(484, "Memories of Krillin")
+MEMORIES_TIEN = GrayCapsule(485, "Memories of Tien")
+MEMORIES_YAMCHA = GrayCapsule(486, "Memories of Yamcha")
+MEMORIES_HERCULE = GrayCapsule(487, "Memories of Hercule")
+MEMORIES_VIDEL = GrayCapsule(488, "Memories of Videl" )
+MEMORIES_GT_SAIYAMAN = GrayCapsule(489, "Memories of Gt Saiyaman" )
+MEMORIES_ANDROID_16 = GrayCapsule(490, "Memories of Android 16")
+MEMORIES_ANDROID_17 = GrayCapsule(491, "Memories of Android 17" )
+MEMORIES_ANDROID_18 = GrayCapsule(492, "Memories of Android 18" )
+MEMORIES_SUPREME_KAI = GrayCapsule(493, "Memories of Supreme Kai" )
+MEMORIES_KID_GOKU = GrayCapsule(494, "Memories of Kid Goku")
+MEMORIES_BARDOCK = GrayCapsule(495, "Memories of Bardock" )
+MEMORIES_UUB = GrayCapsule(496, "Memories of Uub" )
+MEMORIES_RADITZ = GrayCapsule(497, "Memories of Raditz")
+MEMORIES_NAPPA = GrayCapsule(498, "Memories of Nappa" )
+MEMORIES_RECOOME = GrayCapsule(499, "Memories of Recoome" )
+MEMORIES_GINYU = GrayCapsule(500, "Memories of Captain Ginyu" )
+MEMORIES_FRIEZA = GrayCapsule(501, "Memories of Frieza" )
+MEMORIES_DR_GERO = GrayCapsule(502, "Memories of Dr Gero")
+MEMORIES_CELL = GrayCapsule(503, "Memories of Cell" )
+MEMORIES_DABURA = GrayCapsule(504, "Memories of Dabura" )
+MEMORIES_MAJIN_BUU = GrayCapsule(505, "Memories of Majin Buu")
+MEMORIES_SUPER_BUU = GrayCapsule(506, "Memories of Super Buu")
+MEMORIES_KID_BUU = GrayCapsule(507, "Memories of Kid Buu")
+MEMORIES_COOLER = GrayCapsule(508, "Memories of Cooler" )
+MEMORIES_BROLY = GrayCapsule(509, "Memories of Broly" )
+MEMORIES_OMEGA = GrayCapsule(510, "Memories of Omega Shenron" )
+MEMORIES_SAIBAMEN = GrayCapsule(511, "Memories of Saibamen" )
+MEMORIES_CELL_JR = GrayCapsule(512, "Memories of Cell Jr" )
+MEMORIES_HEROES = GrayCapsule(513, "Memories of Heroes" )
+MEMORIES_SUPPORTERS = GrayCapsule(514, "Memories of Supporters")
+CRYSTAL_BALL_0 = GrayCapsule(515, "Baba's Crystal Ball 000")
+CRYSTAL_BALL_1 = GrayCapsule(516, "Baba's Crystal Ball 001" )
+
+GRAY_CAPSULES = [
+    # Fighters
+    GOKU, KID_GOKU, KID_GOHAN, TEEN_GOHAN, GOHAN, GT_SAIYAMAN, GOTEN, VEGETA, TRUNKS, KID_TRUNKS, KRILLIN, PICCOLO,
+    TIEN, YAMCHA, HERCULE, VIDEL, SUPREME_KAI, UUB, RADITZ, NAPPA, RECOOME, GINYU, FRIEZA, ANDROID_16, ANDROID_17,
+    ANDROID_18, DR_GERO, CELL, MAJIN_BUU, SUPER_BUU, KID_BUU, DABURA, COOLER, BARDOCK, BROLY, OMEGA_SHENRON, SAIBAMAN,
+    CELL_JR,
+    # Training
+    TRAINING_1, TRAINING_2, TRAINING_3, TRAINING_4, TRAINING_5, TRAINING_6, TRAINING_7, TRAINING_8, TRAINING_9,
+    TRAINING_10, TRAINING_11, TRAINING_12,
+    # Cards
+    GREEN_CARD, SILVER_CARD, GOLD_CARD, BLACK_CARD,
+    # Modes
+    TOURNEY_NOVICE, TOURNEY_ADEPT, TOURNEY_ADV, TOURNEY_CELL, DRAGON_ARENA,
+    # Stages
+    TOURNEY_STAGE, TIME_CHAMBER, ARCHIPELAGO, MOUNTAINS, PLAINS, GPA_GOHANS_HOUSE, NAMEK, CELL_RING, SUPREME_KAIS_WORLD,
+    INSIDE_BUU, RED_RIBBON_BASE,
+    # Difficulty
+    GOKUS_WISH, PATH_POWER, ENDLESS_PATH, STRONGEST_TROPHY,
+    # Memories
+    MEMORIES_GOKU, MEMORIES_PICCOLO, MEMORIES_KID_GOHAN, MEMORIES_TEEN_GOHAN, MEMORIES_GOHAN, MEMORIES_VEGETA,
+    MEMORIES_GOTEN, MEMORIES_TRUNKS, MEMORIES_KID_TRUNKS, MEMORIES_KRILLIN, MEMORIES_TIEN, MEMORIES_YAMCHA,
+    MEMORIES_HERCULE, MEMORIES_VIDEL, MEMORIES_GT_SAIYAMAN, MEMORIES_ANDROID_16, MEMORIES_ANDROID_17, MEMORIES_ANDROID_18,
+    MEMORIES_SUPREME_KAI, MEMORIES_KID_GOKU, MEMORIES_BARDOCK, MEMORIES_UUB, MEMORIES_RADITZ, MEMORIES_NAPPA,
+    MEMORIES_RECOOME, MEMORIES_GINYU, MEMORIES_FRIEZA, MEMORIES_DR_GERO, MEMORIES_CELL, MEMORIES_DABURA,
+    MEMORIES_MAJIN_BUU, MEMORIES_SUPER_BUU, MEMORIES_KID_BUU, MEMORIES_COOLER, MEMORIES_BROLY, MEMORIES_OMEGA,
+    MEMORIES_SAIBAMEN, MEMORIES_CELL_JR, MEMORIES_HEROES, MEMORIES_SUPPORTERS, CRYSTAL_BALL_0, CRYSTAL_BALL_1
+]
+
+## I don't know what to do with the Story Reenactment voice clips. They can be unlocked, so they can be "items"
+##todo: figure out Story Reenactment later
+
+# Custom items
+DRAGON_RADAR_GOKU = ItemData(520, "Dragon Radar (Goku)")
+DRAGON_RADAR_KGOHAN = ItemData(521, "Dragon Radar (Kid Gohan)")
+DRAGON_RADAR_TGOHAN = ItemData(522, "Dragon Radar (Teen Gohan)")
+DRAGON_RADAR_GOHAN = ItemData(523, "Dragon Radar (Gohan)")
+DRAGON_RADAR_PICCOLO = ItemData(524, "Dragon Radar (Piccolo)")
+DRAGON_RADAR_KRILLIN = ItemData(525, "Dragon Radar (Krillin)")
+DRAGON_RADAR_TIEN = ItemData(526, "Dragon Radar (Tien)")
+DRAGON_RADAR_VEGETA = ItemData(527, "Dragon Radar (Vegeta)")
+DRAGON_RADAR_YAMCHA = ItemData(528, "Dragon Radar (Yamcha)")
+DRAGON_RADAR_UUB = ItemData(529, "Dragon Radar (Uub)")
+DRAGON_RADAR_BROLY = ItemData(530, "Dragon Radar (Broly)")
+
+CUSTOM_ITEMS = [
+    DRAGON_RADAR_GOKU, DRAGON_RADAR_KGOHAN, DRAGON_RADAR_TGOHAN, DRAGON_RADAR_GOHAN, DRAGON_RADAR_PICCOLO,
+    DRAGON_RADAR_KRILLIN, DRAGON_RADAR_TIEN, DRAGON_RADAR_VEGETA, DRAGON_RADAR_YAMCHA, DRAGON_RADAR_UUB, DRAGON_RADAR_BROLY,
+]
+
+ALL_ITEMS = [
+    *RED_CAPSULES,
+    *GREEN_CAPSULES,
+    *YELLOW_CAPSULES,
+    *GRAY_CAPSULES,
+    # *CUSTOM_ITEMS
+]
+
+
+def get_name_pairs() -> Dict[int, str]:
+    collector = {}
+    for item in ALL_ITEMS:
+        collector[item.name] = item.item_id
+    return collector
+
+
+def get_id_pairs() -> Dict[str, int]:
+    collector = {}
+    for item in ALL_ITEMS:
+        collector[item.item_id] = item.name
+    return collector
+
+
+def item_name_to_id(name) -> int | None:
+    return NAME_PAIRS[name]
+
+
+def item_id_to_name(id) -> str | None:
+    return ID_PAIRS[id]
+
+NAME_PAIRS = get_name_pairs()
+ID_PAIRS = get_id_pairs()
